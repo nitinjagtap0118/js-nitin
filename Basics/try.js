f0ad898ab19e0c7787059695801a933096ba914a
nitinjagtap0118 <110155674+nitinjagtap0118@users.noreply.github.com>
2023-10-07 14:33:55 +0000

    done

--- Basics/try.js
+++ Basics/try.js
@@ -1,1 +1,10 @@
-console.log("try to cheack git");
+// console.log("Hello world");
+
+// let str1= "nitin";
+// let str2= " Jagtap"
+// let str3=str1+str2;
+// console.log(str3);
+
+console.log("1" +2);
+console.log("1" + "2");
+console.log( 2 + 2);
